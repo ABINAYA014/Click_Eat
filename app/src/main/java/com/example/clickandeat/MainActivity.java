5b3e26ba4c7e9d40db57609d97e5468111741238
ABINAYA014 <abiselvarajodc@gmail.com>
2020-11-03 17:13:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/clickandeat/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.clickandeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       TextView t1=(TextView) findViewById(R.id.btn);
+
+         t1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, Signup.class));
+
+            }
+
+        });
+}
+    }
